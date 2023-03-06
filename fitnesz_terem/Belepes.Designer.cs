130013833699ed67ca3f2ecb25261f984809a6d8
GLaura22 <laura.gergelyi@gmail.com>
2023-03-06 12:18:17 +0100

    regisztracio modositas
    felesleges inboxok torlese, gombok fgveinek javitasa

--- fitnesz_terem/Belepes.Designer.cs
+++ fitnesz_terem/Belepes.Designer.cs
@@ -35,8 +35,8 @@
             this.cim = new System.Windows.Forms.Label();
             this.regilabel = new System.Windows.Forms.Label();
             this.groupBoxbelep = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.belepButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBoxbelep.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -44,33 +44,37 @@
             // felhlabel
             // 
             this.felhlabel.AutoSize = true;
-            this.felhlabel.Location = new System.Drawing.Point(29, 37);
+            this.felhlabel.Location = new System.Drawing.Point(20, 22);
+            this.felhlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.felhlabel.Name = "felhlabel";
-            this.felhlabel.Size = new System.Drawing.Size(148, 28);
+            this.felhlabel.Size = new System.Drawing.Size(104, 19);
             this.felhlabel.TabIndex = 0;
             this.felhlabel.Text = "Felhasználónév:";
             // 
             // jelszolabel
             // 
             this.jelszolabel.AutoSize = true;
-            this.jelszolabel.Location = new System.Drawing.Point(29, 128);
+            this.jelszolabel.Location = new System.Drawing.Point(20, 77);
+            this.jelszolabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.jelszolabel.Name = "jelszolabel";
-            this.jelszolabel.Size = new System.Drawing.Size(67, 28);
+            this.jelszolabel.Size = new System.Drawing.Size(47, 19);
             this.jelszolabel.TabIndex = 1;
             this.jelszolabel.Text = "Jelszó:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 80);
+            this.textBox1.Location = new System.Drawing.Point(26, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(325, 34);
+            this.textBox1.Size = new System.Drawing.Size(229, 25);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(37, 172);
+            this.textBox2.Location = new System.Drawing.Point(26, 103);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(325, 34);
+            this.textBox2.Size = new System.Drawing.Size(229, 25);
             this.textBox2.TabIndex = 3;
             // 
             // cim
@@ -81,8 +85,9 @@
             this.cim.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.cim.ForeColor = System.Drawing.Color.White;
             this.cim.Location = new System.Drawing.Point(0, 0);
+            this.cim.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.cim.Name = "cim";
-            this.cim.Size = new System.Drawing.Size(519, 65);
+            this.cim.Size = new System.Drawing.Size(344, 45);
             this.cim.TabIndex = 4;
             this.cim.Text = "Balaton Fitnesz terem";
             this.cim.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -91,11 +96,13 @@
             // 
             this.regilabel.AutoSize = true;
             this.regilabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.regilabel.Location = new System.Drawing.Point(137, 237);
+            this.regilabel.Location = new System.Drawing.Point(96, 142);
+            this.regilabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.regilabel.Name = "regilabel";
-            this.regilabel.Size = new System.Drawing.Size(106, 25);
+            this.regilabel.Size = new System.Drawing.Size(70, 15);
             this.regilabel.TabIndex = 5;
             this.regilabel.Text = "Regisztráció";
+            this.regilabel.Click += new System.EventHandler(this.regilabel_Click);
             // 
             // groupBoxbelep
             // 
@@ -110,42 +117,48 @@
             this.groupBoxbelep.Controls.Add(this.felhlabel);
             this.groupBoxbelep.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBoxbelep.ForeColor = System.Drawing.Color.Transparent;
-            this.groupBoxbelep.Location = new System.Drawing.Point(191, 122);
+            this.groupBoxbelep.Location = new System.Drawing.Point(134, 73);
+            this.groupBoxbelep.Margin = new System.Windows.Forms.Padding(2);
             this.groupBoxbelep.Name = "groupBoxbelep";
-            this.groupBoxbelep.Size = new System.Drawing.Size(401, 307);
+            this.groupBoxbelep.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxbelep.Size = new System.Drawing.Size(281, 184);
             this.groupBoxbelep.TabIndex = 6;
             this.groupBoxbelep.TabStop = false;
             this.groupBoxbelep.Text = "Belépés";
             // 
+            // belepButton
+            // 
+            this.belepButton.ForeColor = System.Drawing.Color.Black;
+            this.belepButton.Location = new System.Drawing.Point(193, 139);
+            this.belepButton.Margin = new System.Windows.Forms.Padding(2);
+            this.belepButton.Name = "belepButton";
+            this.belepButton.Size = new System.Drawing.Size(68, 23);
+            this.belepButton.TabIndex = 8;
+            this.belepButton.Text = "Belépés";
+            this.belepButton.UseVisualStyleBackColor = true;
+            this.belepButton.Click += new System.EventHandler(this.belepButton_Click);
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::fitnesz_terem.Properties.Resources.sulyzo;
-            this.pictureBox1.Location = new System.Drawing.Point(6, -7);
+            this.pictureBox1.Location = new System.Drawing.Point(4, -4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(71, 38);
+            this.pictureBox1.Size = new System.Drawing.Size(50, 23);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
-            // belepButton
-            // 
-            this.belepButton.ForeColor = System.Drawing.Color.Black;
-            this.belepButton.Location = new System.Drawing.Point(276, 232);
-            this.belepButton.Name = "belepButton";
-            this.belepButton.Size = new System.Drawing.Size(97, 39);
-            this.belepButton.TabIndex = 8;
-            this.belepButton.Text = "Belépés";
-            this.belepButton.UseVisualStyleBackColor = true;
-            // 
             // Belepes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(831, 533);
+            this.ClientSize = new System.Drawing.Size(582, 320);
             this.Controls.Add(this.groupBoxbelep);
             this.Controls.Add(this.cim);
             this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Belepes";
             this.Text = "Bejelentkezés";
             this.groupBoxbelep.ResumeLayout(false);
--- fitnesz_terem/Regisztracio.cs
+++ fitnesz_terem/Regisztracio.cs
@@ -162,31 +162,31 @@ namespace fitnesz_terem
         {
             resizeControl(cimRect, cim);
             resizeControl(regicimRect, regicim);
-            resizeControl(label1Rect, label1);
-            resizeControl(label2Rect, label2);
+            
+            
             resizeControl(label3Rect, label3);
             resizeControl(label4Rect, label4);
             resizeControl(label5Rect, label5);
-            resizeControl(label6Rect, label6);
+            
             resizeControl(label7Rect, label7);
             resizeControl(label8Rect, label8);
             resizeControl(label9Rect, label9);
-            resizeControl(veznevboxRect, veznevbox);
-            resizeControl(kernevboxRect, kernevbox);
+            
+            
             resizeControl(kartyakodboxRect, kartyakodbox);
             resizeControl(jelszoboxRect, jelszobox);
             resizeControl(jelszoismboxRect, jelszoismbox);
-            resizeControl(emailboxRect, emailbox);
+            
             resizeControl(lejaratiboxRect, lejaratibox);
             resizeControl(jelszoboxRect, jelszobox);
             resizeControl(felhnevboxRect, felhnevbox);
             resizeControl(bankszamlaboxRect, bankszamlabox);
-            resizeControl(emailboxRect, emailbox);
+            
             resizeControl(lejaratiboxRect, lejaratibox);
-            resizeControl(groupBox1Rect, groupBox1);
+            
             resizeControl(groupBox2Rect, groupBox2);
             resizeControl(groupBox3Rect, groupBox3);
-            resizeControl(pictureBox1Rect, pictureBox1);
+            
             resizeControl(pictureBox2Rect, pictureBox2);
             resizeControl(pictureBox3Rect, pictureBox3);
             resizeControl(button1Rect, button1);
@@ -198,29 +198,29 @@ namespace fitnesz_terem
             FormSize = this.Size;
             cimRect = new Rectangle(cim.Location.X, cim.Location.Y, cim.Width, cim.Height);
             regicimRect = new Rectangle(regicim.Location.X, regicim.Location.Y, regicim.Width, regicim.Height);
-            label1Rect = new Rectangle(label1.Location.X, label1.Location.Y, label1.Width, label1.Height);
-            label2Rect = new Rectangle(label2.Location.X, label2.Location.Y, label2.Width, label2.Height);
+            
+            
             label3Rect = new Rectangle(label3.Location.X, label3.Location.Y, label3.Width, label3.Height);
             label4Rect = new Rectangle(label4.Location.X, label4.Location.Y, label4.Width, label4.Height);
             label5Rect = new Rectangle(label5.Location.X, label5.Location.Y, label5.Width, label5.Height);
-            label6Rect = new Rectangle(label6.Location.X, label6.Location.Y, label6.Width, label6.Height);
+            
             label7Rect = new Rectangle(label7.Location.X, label7.Location.Y, label7.Width, label7.Height);
             label8Rect = new Rectangle(label8.Location.X, label8.Location.Y, label8.Width, label8.Height);
             label9Rect = new Rectangle(label9.Location.X, label9.Location.Y, label9.Width, label9.Height);
-            veznevboxRect = new Rectangle(veznevbox.Location.X, veznevbox.Location.Y, veznevbox.Width, veznevbox.Height);
-            kernevboxRect = new Rectangle(kernevbox.Location.X, kernevbox.Location.Y, kernevbox.Width, kernevbox.Height);
+            
+            
             kartyakodboxRect = new Rectangle(kartyakodbox.Location.X, kartyakodbox.Location.Y, kartyakodbox.Width, kartyakodbox.Height);
             jelszoboxRect = new Rectangle(jelszobox.Location.X, jelszobox.Location.Y, jelszobox.Width, jelszobox.Height);
             jelszoismboxRect = new Rectangle(jelszoismbox.Location.X, jelszoismbox.Location.Y, jelszoismbox.Width, jelszoismbox.Height);
-            emailboxRect = new Rectangle(emailbox.Location.X, emailbox.Location.Y, emailbox.Width, emailbox.Height);
+            
             lejaratiboxRect = new Rectangle(lejaratibox.Location.X, lejaratibox.Location.Y, lejaratibox.Width, lejaratibox.Height);
             jelszoboxRect = new Rectangle(jelszobox.Location.X, jelszobox.Location.Y, jelszobox.Width, jelszobox.Height);
             felhnevboxRect = new Rectangle(felhnevbox.Location.X, felhnevbox.Location.Y, felhnevbox.Width, felhnevbox.Height);
             bankszamlaboxRect = new Rectangle(bankszamlabox.Location.X, bankszamlabox.Location.Y, bankszamlabox.Width, bankszamlabox.Height);
-            groupBox1Rect = new Rectangle(groupBox1.Location.X, groupBox1.Location.Y, groupBox1.Width, groupBox1.Height);
+            
             groupBox2Rect = new Rectangle(groupBox2.Location.X, groupBox2.Location.Y, groupBox2.Width, groupBox2.Height);
             groupBox3Rect = new Rectangle(groupBox3.Location.X, groupBox3.Location.Y, groupBox3.Width, groupBox3.Height);
-            pictureBox1Rect = new Rectangle(pictureBox1.Location.X, pictureBox1.Location.Y, pictureBox1.Width, pictureBox1.Height);
+            
             pictureBox2Rect = new Rectangle(pictureBox2.Location.X, pictureBox2.Location.Y, pictureBox2.Width, pictureBox2.Height);
             pictureBox3Rect = new Rectangle(pictureBox3.Location.X, pictureBox3.Location.Y, pictureBox3.Width, pictureBox3.Height);
             button1Rect = new Rectangle(button1.Location.X, button1.Location.Y, button1.Width, button1.Height);
