a627c5c5c87c8586f264fbaf924d259cb3c153ed
Holler János <hollerjanika@gmail.com>
2023-03-13 02:18:28 +0100

    Új controller hozzáadása
    RoleController hozzáadása a repository-hoz egy GetRolyByID statikus metódussal, mely vissza adja az összes metódust.

---
+++ fitnesz_terem/Database_Backend/Controllers/RoleController.cs
@@ -0,0 +1,31 @@
+﻿using fitnesz_terem.Database_Backend.Connection;
+using fitnesz_terem.Database_Backend.Modells_Tables;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace fitnesz_terem.Database_Backend.Controllers
+{
+    public class RoleController
+    {
+        public static string GetRoleByID(int roleID)
+        {
+            try
+            {
+                /* SQL Connection. */
+                var context = new FitnessDbContext();
+
+                var role = context.Roles
+                    .Single(row => row.RoleID == roleID);
+
+                /* Return the previously saved ID. */
+                return role.Label;
+            }
+            catch (Exception)
+            {
+                /* On error. */
+                return "";
+            }
+        }
+    }
+}
